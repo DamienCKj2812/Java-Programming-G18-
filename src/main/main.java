62d998cb9c8d88a49de10b65e07b69db20c61c12
Damien <chongkahjun@simple.com>
2024-03-21 19:24:26 +0800

    Final version

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+package main;
+                        
+
+
+public class main {
+
+public static void main(String[] args) {
+    RoleAuthUI roleAuthUI = new RoleAuthUI();
+    roleAuthUI.LoginUI();
+
+    }
+
+}
+
+
+
+
+
+
